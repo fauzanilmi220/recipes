ea6d86d3b75c88038669278e59e6bec258230523
fauzanilmi220 <fauzanilmi220@gmail.com>
2023-06-01 15:59:27 +0700

    edit sql

--- recipes.sql
+++ recipes.sql
@@ -13,8 +13,6 @@ CREATE TABLE users(
     role VARCHAR NOT NULL
 );
 
-SELECT * FROM users;
-
 --RECIPE
 CREATE TABLE recipe(
     id SERIAL PRIMARY KEY, 
@@ -27,37 +25,9 @@ CREATE TABLE recipe(
     deleted_at TIMESTAMP DEFAULT NULL
 );
 
-SELECT * FROM recipe;
-
-SELECT * FROM recipe where deleted_at IS NOT NULL;
-
-SELECT * FROM recipe where deleted_at IS NULL;
-
-SELECT * FROM recipe ORDER BY created_at DESC OFFSET 1 LIMIT 10;
-
-UPDATE recipe SET photo = NULL WHERE name = 'lele';
-
-ALTER TABLE recipe add deleted_at TIMESTAMP DEFAULT NULL;
-
-ALTER TABLE recipe DROP COLUMN users_id;
-
-ALTER TABLE recipe add users_id VARCHAR;
-
-ALTER TABLE recipe add Foreign Key (users_id) REFERENCES users(id);
-
 --CATEGORY
 CREATE TABLE category(
     id SERIAL PRIMARY KEY, 
     name VARCHAR,
     deleted_at TIMESTAMP DEFAULT NULL
 );
-
-SELECT * FROM category;
-
-INSERT INTO category(name) VALUES('Main Dish');
-
-UPDATE category SET name = 'Dessert' WHERE id = 1;
-
-ALTER TABLE category add deleted_at TIMESTAMP DEFAULT NULL;
-
-DELETE FROM category WHERE name = 'undefined';
